03ffcd7f48b01e03dc9b4cec883b87b8d6e9aa87
Hisham Al Ahmad <hishamalahmadx@gmail.com>
2025-02-24 22:17:10 +0200

    Updating the NotFound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,14 @@
 const NotFound = () => {
     return ( 
-        <div>
-            <img src="img/404.jpg" alt="Page not found" style={{width: "25%", height: "25", marginLeft: "40%"}} />
-            <h1 style={{marginLeft: "40%", marginRight: "25%"}}>404 Not Found</h1>
+        <div className="container text-center" >
+            <img src="/img/404.gif" alt="404 Error" className="img-fluid mb-4" style={{maxWidth: "350px"}} />
+            {/* <h1 className="display-1">404</h1> */}
+            <h2 className="display-4">Page Not Found</h2>
+            <p className="lead">We are sorry, but the page you requested was not found</p>
+            <a href="/" className="btn btn-primary py-sm-3 px-sm-5 me-3">Go to Home</a>
         </div>
      );
 }
+
  
 export default NotFound;
