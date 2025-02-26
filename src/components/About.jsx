45463d6d9f8903b6ed65628e5ae233e426b11462
Hisham Al Ahmad <hishamalahmadx@gmail.com>
2025-02-26 16:13:26 +0200

    Updating spinner

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,9 @@ const About = () => {
                         <div className="col-lg-6">
                             <div className="row g-3">
                                 <div className="col-6 text-start">
-                                    <img className="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" />
+                                    <img className="img-fluid rounded w-100 wow zoomIn x" data-wow-delay="0.1s" src="img/about.jpg"
+                                        style={{aspectRatio: "1/1", objectFit: "cover"}}
+                                    />
                                 </div>
                                 <div className="col-6 text-start">
                                     <img className="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.25s" src="img/about-2.jpg" style={{marginTop: "25%"}} />
@@ -25,27 +27,7 @@ const About = () => {
                             <h1 className="mb-4"><i>Welcome to Fresh Time</i></h1>
                             <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos erat ipsum et lorem et sit, sed stet lorem sit.</p>
                             <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                            <div className="row g-4 mb-4">
-                                <div className="col-sm-6">
-                                    <div className="d-flex align-items-center border-start border-5 border-primary px-3">
-                                        <h1 className="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">15</h1>
-                                        <div className="ps-4">
-                                            <p className="mb-0">Years of</p>
-                                            <h6 className="text-uppercase mb-0">Experience</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="d-flex align-items-center border-start border-5 border-primary px-3">
-                                        <h1 className="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">50</h1>
-                                        <div className="ps-4">
-                                            <p className="mb-0">Popular</p>
-                                            <h6 className="text-uppercase mb-0">Master Chefs</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a className="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                            <a className="btn btn-primary py-3 px-5 mt-2" href="#">Read More</a>
                         </div>
                     </div>
                 </div>
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
 const Spinner = () => {
-    return ( 
-        <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div className="spinner-border text-primary" style={{width: "3rem", height: "3rem"}} role="status">
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 1500); 
+
+        return () => clearTimeout(timer); 
+    }, []);
+
+    return (
+        <div id="spinner" className={`spinner ${isVisible ? 'show' : ''}`}>
+            <div className="spinner-border text-primary" style={{ width: "3rem", height: "3rem" }} role="status">
                 <span className="sr-only">Loading...</span>
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Spinner;
