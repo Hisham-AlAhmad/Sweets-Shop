4c820cc1d7641e1d70dfb115bc4c50ba9f8d938e
Hisham Al Ahmad <hishamalahmadx@gmail.com>
2025-03-06 09:16:33 +0200

    Updating the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,71 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
 import "./navbar.css";
+
 const Navbar = () => {
+    const location = useLocation();
+    const navbarCollapseRef = useRef(null);
+    
+    // Function to close the navbar
+    const closeNavbar = () => {
+        if (navbarCollapseRef.current && navbarCollapseRef.current.classList.contains('show')) {
+            // Check if Bootstrap is available
+            if (typeof bootstrap !== 'undefined') {
+                const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapseRef.current);
+                if (bsCollapse) {
+                    bsCollapse.hide();
+                }
+            }
+        }
+    };
+    
+    // Close navbar when route changes
+    useEffect(() => {
+        closeNavbar();
+    }, [location]);
+    
+    // Close navbar when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                navbarCollapseRef.current && 
+                !navbarCollapseRef.current.contains(event.target) &&
+                !event.target.classList.contains('navbar-toggler') &&
+                !event.target.classList.contains('fa-bars')
+            ) {
+                closeNavbar();
+            }
+        };
+        
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top shadow-sm px-4 px-lg-5 py-lg-0">
             {/* Logo */}
             <NavLink to="/" className="navbar-brand p-0 d-flex align-items-center me-auto">
-                <img src="/img/freshTime_noBg.png" alt="logo image" className="img-fluid" style={{ maxHeight: '100px' }} />
+                <img src="/img/freshTime_noBg.png" alt="FreshTime logo" className="img-fluid" />
             </NavLink>
 
             {/* Toggler Button for Mobile */}
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarCollapse" 
+                aria-controls="navbarCollapse" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
                 <span className="fa fa-bars"></span>
             </button>
 
             {/* Links */}
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <div className="navbar-nav ms-auto py-0 pe-4">
+            <div className="collapse navbar-collapse" id="navbarCollapse" ref={navbarCollapseRef}>
+                <div className="navbar-nav ms-auto py-0 pe-4 text-center">	
                     <NavLink to="/"
                         className={({ isActive }) =>
                             `nav-item nav-link ${isActive ? 'active' : ''}`
@@ -40,6 +90,6 @@ const Navbar = () => {
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
