ea2b64d6e2db9f6c556a9648242d574ffa03e452
Hisham Al Ahmad <hishamalahmadx@gmail.com>
2025-05-14 09:39:12 +0300

    Adding go back btn in notFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
-    return ( 
+    const navigate = useNavigate();
+
+    return (
         <div className="container text-center" >
-            <img src="/img/404.gif" alt="404 Error" className="img-fluid mb-4" style={{maxWidth: "350px"}} />
+            <img src="/img/404.gif" alt="404 Error" className="img-fluid mb-4" style={{ maxWidth: "350px" }} />
             {/* <h1 className="display-1">404</h1> */}
             <h2 className="display-4">Page Not Found</h2>
             <p className="lead">We are sorry, but the page you requested was not found</p>
-            <a href="/" className="btn btn-primary py-sm-3 px-sm-5 me-3">Go to Home</a>
+            <button
+                className="btn btn-primary py-sm-3 px-sm-5 me-3"
+                onClick={() => navigate(-1)}
+            >Go Back
+            </button>
         </div>
-     );
+    );
 }
 
- 
+
 export default NotFound;
